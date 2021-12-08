e20e71b3889a3913db7e42fbd553c06d40543ec7
Bussard <j-senger@gmx.de>
2021-12-08 22:38:08 +0100

    commit.

---
+++ cmake-build-debug/CMakeFiles/A03_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A03_2.exe"
+  "A03_2.exe.manifest"
+  "A03_2.lib"
+  "A03_2.pdb"
+  "CMakeFiles/A03_2.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A03_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int array[], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (array[j] > array[j + 1])
+                swap(&array[j], &array[j + 1]);
+}
+
+void printArray(int array[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", array[i]);
+    printf(";");
+}
+
+int main() {
+    printf("Executing bubble sort on unsorted integer array...\n");
+    int array[] = {89, 32, 20, 113, -15};
+    int size = sizeof(array) / sizeof(array[0]);
+    printf("Unsorted array : ");
+    printArray(array, size);
+    printf("\n");
+    bubbleSort(array, size);
+    printf("Sorted array: ");
+    printArray(array, size);
+    return 0;
+}
